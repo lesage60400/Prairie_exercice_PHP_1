5897885ff351f7f37d531c31a37a45f15bc75166
lesage60400 <lesage.pierre@neuf.fr>
2016-05-09 13:42:43 +0200

    Exercice 1 prairie du 09/05/2016 PHP

---
+++ exercice1.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<title>Exercice 1 PHP de la prairie</title>
+</head>
+<body>
+<?php
+$nombre1=15;
+$nombre2=10;
+$addition=$nombre1+$nombre2;
+$multiplication=$nombre1*$nombre2;
+echo $addition;
+echo "<br>";
+echo $multiplication;
+?>
+</body>
+</html>
